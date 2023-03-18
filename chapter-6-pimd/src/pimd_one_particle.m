0be5f4bd41cfd94bff301ba522b2eb5a5be8e9ea
Zheyong Fan <brucenju@gmail.com>
2023-03-19 05:59:53 +0800

    both PIMD and PRMD finished

--- chapter-6-pimd/src/pimd_one_particle.m
+++ chapter-6-pimd/src/pimd_one_particle.m
@@ -1,6 +1,6 @@
 clear; close all;
-n_beads=8; beta=1; hbar=1; m=1; lambda=5; dt = 0.01; 
-omega_n=n_beads/beta/hbar; n_step=200;
+n_beads=8; beta=1; hbar=1; m=1; lambda=5; dt = 0.01; tau_T=100;
+omega_n=n_beads/beta/hbar; n_step=200000; n_step_pimd=100000;
 C=zeros(n_beads,n_beads);
 for j=1:n_beads
     for k=0:n_beads-1
@@ -15,8 +15,17 @@ for j=1:n_beads
         end
     end
 end
-p=linspace(-2,-1,n_beads); q=linspace(-1,1,n_beads); qq=zeros(n_step,n_beads);
-for step=1:n_step
+p=linspace(-2,-1,n_beads); q=linspace(-1,1,n_beads); 
+pp=zeros(n_step,n_beads); qq=zeros(n_step,n_beads);
+for step=1:n_step   
+    p_normal=p*C;
+    c1=exp(-dt*omega_n*sin((0:n_beads-1)*pi/n_beads));
+    if step<=n_step_pimd
+        c1(1)=exp(-1/2/tau_T);
+    end
+    c2=sqrt(1-c1.^2);
+    p_normal=c1.*p_normal+sqrt(n_beads*m/beta)*c2.*randn(1,n_beads);
+    p=(C*p_normal.').';
     p=p-(dt/2)*m*lambda*lambda*q; 
     p_normal=p*C; q_normal=q*C;
     for k=0:n_beads-1
@@ -33,12 +42,29 @@ for step=1:n_step
     end
     p=(C*p_normal.').'; q=(C*q_normal.').';
     p=p-(dt/2)*m*lambda*lambda*q;
-    qq(step,:)=q;
+    p_normal=p*C;
+    c1=exp(-dt*omega_n*sin((0:n_beads-1)*pi/n_beads));
+    if step<=n_step_pimd
+        c1(1)=exp(-1/2/tau_T);
+    end
+    c2=sqrt(1-c1.^2);
+    p_normal=c1.*p_normal+sqrt(n_beads*m/beta)*c2.*randn(1,n_beads);
+    p=(C*p_normal.').';
+    pp(step,:)=p; qq(step,:)=q;
 end
 figure;
-plot((1:n_step)*dt,mean(qq,2),'r-','linewidth',3);hold on
 plot((1:n_step)*dt,qq,'linewidth',0.5);hold on;
+plot((1:n_step)*dt,mean(qq,2),'r-','linewidth',3);hold on
 xlabel('time');
 ylabel('position');
-legend('centroid');
+set(gca,'fontsize',16);
+figure;
+plot(mean(qq(1:end/2,:),2),mean(pp(1:end/2,:),2),'.','markersize',20);
+xlabel('position');
+ylabel('momentum');
+set(gca,'fontsize',16);
+figure;
+plot(mean(qq(end/2+1:end,:),2),mean(pp(end/2+1:end,:),2),'.','markersize',20);
+xlabel('position');
+ylabel('momentum');
 set(gca,'fontsize',16);
