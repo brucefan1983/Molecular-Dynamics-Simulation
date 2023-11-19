7ffeaf502fb4d4d235b59992f4a2f0c9631548ff
brucefan1983 <brucenju@gmail.com>
2023-11-19 19:11:41 +0800

    remove unused files

--- chapter-20-lsqt/src/lsqt-matlab/create_state.m
+++
@@ -1,4 +0,0 @@
-function phi=create_state(N)
-random_phase=rand(N,1)*2*pi;
-phi=cos(random_phase)+sin(random_phase)*1i;   
-phi=phi/norm(phi);
--- chapter-20-lsqt/src/lsqt-matlab/find_dos.m
+++
@@ -1,3 +0,0 @@
-function DOS=find_dos(M,E_max,E_scaled,H_scaled,phi)
-C=find_moments(M,H_scaled,phi,phi);
-DOS=chebyshev_summation(M,C,E_scaled,E_max);
