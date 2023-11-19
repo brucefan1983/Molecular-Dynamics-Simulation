8b8495287c41cc0454fa93f7e764d6995cfef48f
brucefan1983 <brucenju@gmail.com>
2023-11-19 19:33:24 +0800

    clean up chapter 6's code

--- chapter-06-simple_md/src/ex1/plot_results.py
+++ chapter-06-simple_md/src/ex1/plot_results.py
@@ -8,7 +8,7 @@ timeInterval = timeStep * sampleInterval
 numData = thermo.shape[0]
 time = np.arange(1, numData + 1) * timeInterval
 totalEnergy = thermo[:, 1] + thermo[:, 2]
-relativeEnergy = totalEnergy / np.mean(np.abs(totalEnergy))
+relativeEnergy = (totalEnergy - np.mean(totalEnergy)) / np.abs(np.mean(totalEnergy))
 
 plt.figure(figsize=(12, 8))
 
@@ -33,16 +33,9 @@ plt.title('(c)', fontsize=15)
 plt.subplot(2, 2, 4)
 plt.plot(time[1:], relativeEnergy[1:], '-', linewidth=2)
 plt.xlabel('Time (ps)', fontsize=15)
-plt.ylabel('Relative Energy', fontsize=15)
+plt.ylabel('Relative fluctuations', fontsize=15)
+plt.ylim((-1e-4,1e-4))
 plt.title('(d)', fontsize=15)
 
 plt.tight_layout()
 plt.savefig('fig-chapter-6-energy.pdf')
-
-plt.figure()
-plt.plot(time, thermo[:, 0], '-', linewidth=2)
-plt.xlabel('Time (ps)', fontsize=15)
-plt.ylabel('Temperature (K)', fontsize=15)
-
-plt.tight_layout()
-plt.savefig('fig-chapter-6-temperature.pdf')
