97e59ca8dae7b1ccbcc1de0e68b01336646a80b5
brucefan1983 <brucenju@gmail.com>
2023-12-01 01:00:11 +0800

    example for comparing barostats

---
+++ chapter-07-barostat/compare_barostats/compare_thermostats.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+thermo_ber=np.loadtxt('ber/thermo.out')
+thermo_scr=np.loadtxt('scr/thermo.out')
+thermo_mttk=np.loadtxt('mttk/thermo.out')
+
+M=thermo_ber.shape[0]
+t=np.arange(1,M+1)*0.001 # ps
+
+    
+plt.figure(figsize=(8, 8))
+
+plt.subplot(2, 1, 1)
+plt.plot(t, np.mean(thermo_mttk[:,3:5],axis=1), '-', linewidth=2,label='MTTK')
+plt.plot(t, np.mean(thermo_scr[:,3:5],axis=1), '-', linewidth=2,label='SCR')
+plt.plot(t, np.mean(thermo_ber[:,3:5],axis=1), '-', linewidth=2,label='Berendsen')
+plt.xlabel('Time (ps)', fontsize=15)
+plt.ylabel('Pressure (GPa)', fontsize=15)
+plt.legend(fontsize=15)
+
+
+plt.subplot(2, 1, 2)
+plt.plot(t, np.mean(thermo_mttk[:,9:12],axis=1), '-', linewidth=2,label='MTTK')
+plt.plot(t, np.mean(thermo_scr[:,9:12],axis=1), '-', linewidth=2,label='SCR')
+plt.plot(t, np.mean(thermo_ber[:,9:12],axis=1), '-', linewidth=2,label='Berendsen')
+plt.xlabel('Time (ps)', fontsize=15)
+plt.ylabel('Length (A)', fontsize=15)
+plt.legend(fontsize=15)
+
+plt.tight_layout()
+plt.savefig('fig-barostat-compare.pdf')
+
+
