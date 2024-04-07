28a0f26009ca3f4da19bec48f0071818675e14b8
brucefan1983 <brucenju@gmail.com>
2024-04-07 23:49:05 +0800

    add emd kappa example

---
+++ chapter-09-transport_properties/GPUMD_silicon_kappa/emd/plot_emd.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+hac=np.loadtxt('hac.out')
+
+M=5000
+Ns=hac.shape[0]//M
+t=hac[0:M,0] # ps
+kappa=np.sum(hac[:,6:],axis=1)/3
+kappa=kappa.reshape((Ns,M))
+
+print('Ns=',Ns)
+print('kappa=',np.mean(kappa[:,-1]),'+-',np.std(kappa[:,-1])/np.sqrt(Ns))
+
+    
+plt.figure(figsize=(8, 5))
+plt.plot(t, kappa.transpose(), '--',color=(0.7,0.7,0.7), linewidth=1)
+plt.plot(t, np.mean(kappa,axis=0), '-', linewidth=3)
+plt.xlabel('Correlation time (ps)', fontsize=15)
+plt.ylabel('$\kappa$ (W m$^{-1}$ K$^{-1}$)', fontsize=15)
+plt.tight_layout()
+plt.savefig('fig-c9-kappa-emd.pdf')
+
+
