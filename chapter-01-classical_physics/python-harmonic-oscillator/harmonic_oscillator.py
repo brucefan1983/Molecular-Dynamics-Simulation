d6a2b4b1bb451fccf21fdb29841b09f9432dc2df
brucefan1983 <brucenju@gmail.com>
2024-04-16 22:09:58 +0800

    improve a python code

--- chapter-01-classical_physics/python-harmonic-oscillator/harmonic_oscillator.py
+++ chapter-01-classical_physics/python-harmonic-oscillator/harmonic_oscillator.py
@@ -5,12 +5,12 @@ m=1; k=1; dt=0.01; n_step=1000
 v=0; x=1
 v_vector = np.zeros(n_step)
 x_vector = np.zeros(n_step)
-for step in range(1, n_step+1):
-    v = v - (dt/2) * k * x
+for step in range(n_step):
+    v = v + (dt/2) * (-k * x / m)
     x = x + dt * v
-    v = v - (dt/2) * k * x
-    v_vector[step-1] = v
-    x_vector[step-1] = x
+    v = v + (dt/2) * (-k * x / m)
+    v_vector[step] = v
+    x_vector[step] = x
 
 plt.figure()
 plt.plot(np.arange(1, n_step+1) * dt, x_vector, linewidth=2)
@@ -18,7 +18,7 @@ plt.plot(np.arange(1, n_step+1) * dt, v_vector, '--', linewidth=2)
 plt.xlabel('time')
 plt.ylabel('position or velocity')
 plt.legend(['position', 'velocity'])
-plt.savefig('fig-chapter-1-position_and_momentum.pdf')
+plt.savefig('fig-c01-position_and_momentum.pdf')
 
 potential = 0.5 * k * x_vector**2
 kinetic = 0.5 * m * v_vector**2
@@ -26,14 +26,17 @@ kinetic = 0.5 * m * v_vector**2
 plt.figure()
 plt.plot(np.arange(1, n_step+1) * dt, potential, linewidth=2)
 plt.plot(np.arange(1, n_step+1) * dt, kinetic, '--', linewidth=2)
-plt.plot(np.arange(1, n_step+1) * dt, potential + kinetic, '-.', linewidth=3)
+plt.plot(np.arange(1, n_step+1) * dt, potential + kinetic, '-.', linewidth=1)
 plt.xlabel('time')
 plt.ylabel('Energy')
 plt.legend(['potential', 'kinetic', 'total'])
-plt.savefig('fig-chapter-1-energy_conservation.pdf')
+plt.savefig('fig-c01-energy_conservation.pdf')
 
 plt.figure()
-plt.plot(x_vector, v_vector, '.', markersize=20)
+
+plt.figure()
+plt.plot(x_vector, v_vector, '.', markersize=10)
 plt.xlabel('position')
 plt.ylabel('momentum')
-plt.savefig('fig-chapter-1-phase_space.pdf', dpi=200)
+plt.axis('equal')
+plt.savefig('fig-c01-phase_space.pdf', dpi=200)
