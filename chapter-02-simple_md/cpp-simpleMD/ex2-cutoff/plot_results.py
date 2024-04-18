5c7ee13b81c6e000c0e6511b85a14a97f659598a
brucefan1983 <brucenju@gmail.com>
2024-04-19 03:46:57 +0800

    improve a figure

--- chapter-02-simple_md/cpp-simpleMD/ex2-cutoff/plot_results.py
+++ chapter-02-simple_md/cpp-simpleMD/ex2-cutoff/plot_results.py
@@ -13,11 +13,12 @@ for n in range(4):
     fluctuation_value[n] = np.std((energy - np.mean(energy))/np.abs(energy))
     print(fluctuation_value[n])
 
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(8, 7))
 
-plt.semilogy(np.array((7,9,12,15)), fluctuation_value, '-o', linewidth=2)
-plt.xlabel('Cutoff radius ($\mathrm{\AA}$)', fontsize=15)
-plt.ylabel('Magnitude of relative energy fluctuations', fontsize=15)
-
+plt.semilogy(np.array((7,9,12,15)), fluctuation_value, '-o', linewidth=2,markersize=10)
+plt.xlabel('Cutoff radius ($\mathrm{\AA}$)', fontsize=20)
+plt.ylabel('Magnitude of relative energy fluctuations', fontsize=20)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
 plt.tight_layout()
 plt.savefig('fig-c02-cutoff.pdf')
