fee029938f7184beb9945a3aef6da4e23152cbf4
brucefan1983 <brucenju@gmail.com>
2024-04-19 02:17:37 +0800

    improve a figure

--- chapter-02-simple_md/python-LJ-potential/plot_potential.py
+++ chapter-02-simple_md/python-LJ-potential/plot_potential.py
@@ -21,15 +21,15 @@ fig, ax1 = plt.subplots()
 
 color = "tab:blue"
 ax1.plot(r, U(r, rc), color=color)
-ax1.hlines(0, r.min(), r.max(), linestyles="dashed", colors="k", alpha=0.5, zorder=-1)
+ax1.hlines(0, r.min(), r.max(), linestyles="dotted", colors="k", alpha=0.5, zorder=-1)
 ax1.set_xlabel(r"Distance ($\sigma$)")
 ax1.set_ylabel(r"Energy ($\epsilon$)", color=color)
 ax1.set_ylim(-1.2, 1.2)
 
 color = "tab:orange"
 ax2 = ax1.twinx()
-ax2.plot(r, F(r, rc), color=color)
+ax2.plot(r, F(r, rc), '--',color=color)
 ax2.set_ylabel(r"Force ($\epsilon$/$\sigma$)", color=color)
 ax2.set_ylim(-25, 25)
 
-plt.savefig("fig-chapter-6-lj.pdf")
+plt.savefig("fig-c02-lj.pdf")
