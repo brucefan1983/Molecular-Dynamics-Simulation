6ab2d5e6fc790ce4ecd3a8e2d4d48a847bf6b2d3
brucefan1983 <brucenju@gmail.com>
2024-05-08 13:41:25 +0800

    improve figs in chapter 7

--- chapter-07-barostat/compare_barostats/compare_thermostats.py
+++ chapter-07-barostat/compare_barostats/compare_thermostats.py
@@ -9,26 +9,28 @@ M=thermo_ber.shape[0]
 t=np.arange(1,M+1)*0.001 # ps
 
     
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(12, 6))
 
-plt.subplot(2, 1, 1)
+plt.subplot(1, 2, 1)
 plt.plot(t, np.mean(thermo_mttk[:,3:5],axis=1), '-', linewidth=2,label='MTTK')
 plt.plot(t, np.mean(thermo_scr[:,3:5],axis=1), '-', linewidth=2,label='SCR')
 plt.plot(t, np.mean(thermo_ber[:,3:5],axis=1), '-', linewidth=2,label='Berendsen')
 plt.xlabel('Time (ps)', fontsize=15)
 plt.ylabel('Pressure (GPa)', fontsize=15)
+plt.title('(a)', fontsize=15)
 plt.legend(fontsize=15)
 
 
-plt.subplot(2, 1, 2)
+plt.subplot(1, 2, 2)
 plt.plot(t, np.mean(thermo_mttk[:,9:12],axis=1), '-', linewidth=2,label='MTTK')
 plt.plot(t, np.mean(thermo_scr[:,9:12],axis=1), '-', linewidth=2,label='SCR')
 plt.plot(t, np.mean(thermo_ber[:,9:12],axis=1), '-', linewidth=2,label='Berendsen')
 plt.xlabel('Time (ps)', fontsize=15)
-plt.ylabel('Length (A)', fontsize=15)
+plt.ylabel('Length ($\mathrm{\AA}$)', fontsize=15)
+plt.title('(b)', fontsize=15)
 plt.legend(fontsize=15)
 
 plt.tight_layout()
-plt.savefig('fig-barostat-compare.pdf')
+plt.savefig('fig-c07-barostat-compare.pdf')
 
 
--- chapter-07-barostat/npt_examples/npt_scr_3/plot_results.py
+++ chapter-07-barostat/npt_examples/npt_scr_3/plot_results.py
@@ -7,26 +7,28 @@ M=thermo.shape[0]
 t=np.arange(1,M+1)*0.002 # ps
 
     
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(12, 6))
 
-plt.subplot(2, 1, 1)
+plt.subplot(1, 2, 1)
 plt.plot(t, thermo[:,3], '-', linewidth=2,label='x')
 plt.plot(t, thermo[:,4], '-', linewidth=2,label='y')
 plt.plot(t, thermo[:,5], '-', linewidth=2,label='z')
 plt.xlabel('Time (ps)', fontsize=15)
 plt.ylabel('Pressure (GPa)', fontsize=15)
+plt.title('(a)', fontsize=15)
 plt.legend(fontsize=15)
 
 
-plt.subplot(2, 1, 2)
+plt.subplot(1, 2, 2)
 plt.plot(t, thermo[:,9], '-', linewidth=2,label='x')
 plt.plot(t, thermo[:,10], '-', linewidth=2,label='y')
 plt.plot(t, thermo[:,11], '-', linewidth=2,label='z')
 plt.xlabel('Time (ps)', fontsize=15)
-plt.ylabel('Length (A)', fontsize=15)
+plt.ylabel('Length ($\mathrm{\AA}$)', fontsize=15)
+plt.title('(b)', fontsize=15)
 plt.legend(fontsize=15)
 
 plt.tight_layout()
-plt.savefig('fig-npt-examples-1.pdf')
+plt.savefig('fig-c07-npt-examples-1.pdf')
 
 
