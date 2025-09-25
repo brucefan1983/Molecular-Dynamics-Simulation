67f9d2e8cbf29e3db251df385b9963d97ae1521d
duanzaixu <banker9@126.com>
2025-09-25 19:42:52 +0800

    Update loadtxt dir : chapter-04-empirical_potentials/gpumd-tersoff/plot_force.py line 5

--- chapter-04-empirical_potentials/gpumd-tersoff/plot_force.py
+++ chapter-04-empirical_potentials/gpumd-tersoff/plot_force.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 force = np.loadtxt('force.out')
-force_cpp = np.loadtxt('../code-tersoff-validation/force.out')
+force_cpp = np.loadtxt('../cpp-tersoff-validation/force.out')
 
 plt.figure()
 plt.plot(force[0:64,:]-force_cpp[0:64,:], 'o')
